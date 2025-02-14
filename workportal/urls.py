c770b56c91a728659421e93983efd27a0896c539
dydgjs200 <dydgjs2016@naver.com>
2025-02-14 14:11:37 +0900

    routing practice

--- workportal/urls.py
+++ workportal/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from apps.accounts.views import hello_world
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world, name='hello_world')
+    path('accounts/', include('apps.accounts.urls'))
 ]
