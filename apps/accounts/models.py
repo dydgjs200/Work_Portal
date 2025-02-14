5de4a600d33cbf12c7242a71e18696080014d67a
dydgjs200 <dydgjs2016@naver.com>
2025-02-14 22:40:00 +0900

    id and createdAt

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=100)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
