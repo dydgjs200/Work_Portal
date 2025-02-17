9923c8b58d4fb5cd674e406fbf05c463882af331
dydgjs200 <dydgjs2016@naver.com>
2025-02-17 12:41:14 +0900

    weblink table 생성

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Account(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20, unique=True)
--- apps/feeds/models.py
+++ apps/feeds/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class WebLink(models.Model):
+    CATEGORY = [
+        ('personal', '개인 즐겨 찾기'),
+        ('work', '업무 활용 자료'),
+        ('refer', '참고 자료'),
+        ('edu', '교육 및 학습 자료'),
+        ('others', '기타 자료')
+    ]
+    id = models.AutoField(primary_key=True)
+    created_by = models.CharField(max_length=20, unique=True)       # 작성한 사용자
+    name = models.CharField(max_length=100)     # 웹 링크 이름
+    url = models.CharField(max_length=200)      # 주소 길이
+    category = models.CharField(max_length=20, choices=CATEGORY, default='personal')
+
+    def __str__(self):
+        return self.username
