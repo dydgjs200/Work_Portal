170d82739e55c77a7f16d965fbc55c61111a004b
dydgjs200 <dydgjs2016@naver.com>
2025-02-17 13:49:53 +0900

    피드 추가 전 계정이 유효한지 검증

--- apps/feeds/views.py
+++ apps/feeds/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import WebLink
 from django.views.decorators.csrf import csrf_exempt
+from apps.accounts.models import Account
+
 
 @csrf_exempt
 def add_Feed(request):
@@ -13,6 +15,10 @@ def add_Feed(request):
             name = data.get('name')
             url = data.get('url')
             category = data.get('category')
+
+            # 아이디가 유저(Accounts)에 있는지 확인 필요
+            if not Account.objects.filter(username = created_by).exists():
+                return JsonResponse({'error': 'User does not exist'}, status=404)
 
             feeds = WebLink.objects.create(created_by = created_by, name = name, url = url, category = category)
 
