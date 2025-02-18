4dd731829f246e22f9396f505c30c9e352cce730
dydgjs200 <dydgjs2016@naver.com>
2025-02-18 12:53:29 +0900

    edit feeds

--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import add_Feed
+from .views import add_feed, edit_feed, delete_feed
 
 urlpatterns = [
-    path('addfeed/', add_Feed, name = "add_Feed")
+    path('addfeed/', add_feed, name = "add_feed"),
+    path('editfeed/', edit_feed, name = "edit_feed"),
+    path('deletefeed/', delete_feed, name = "delete_feed")
 ]
--- apps/feeds/views.py
+++ apps/feeds/views.py
@@ -6,7 +6,7 @@ from apps.accounts.models import Account
 from .models import WebLink
 
 @csrf_exempt
-def add_Feed(request):
+def add_feed(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
@@ -29,3 +29,48 @@ def add_Feed(request):
             return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
     return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+@csrf_exempt
+def edit_Feed(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            created_by = data.get('created_by')  # 요청에서 username 가져오기
+            feed_id = data.get('feed_id')
+            name = data.get('name')
+            url = data.get('url')
+            category = data.get('category')
+
+            # username이 존재하는지 확인하고, role도 함께 가져오기
+            user = get_object_or_404(Account, username=created_by)
+            user_role = user.role  # role 속성 가져오기
+
+            # 유저의 권한이 충분하지 않다면 비승인
+            if user_role != "admin":
+                return JsonResponse({'message' : "Unauthorized User"}, status = 403)
+            
+            # 수정할 feed 가져오기
+            feed = get_object_or_404(WebLink, id=feed_id)
+
+            # feed 내용 업데이트
+            feed.name = name
+            feed.url = url
+            feed.category = category
+            feed.save()
+
+            return JsonResponse({'message': 'Feed updated successfully', 'role': user_role}, status=200)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+
+@csrf_exempt
+def delete_feed(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+
+        except:
+            return
